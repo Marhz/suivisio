1cd517e478fa6391dd623d189f5ba15996a7d4f2
Boris on braavos <alexandre.mesle@gmail.com>
2017-10-22 17:56:28 +0200

    admin credentials in .env.example

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       DB::table('users')->insert([
-          'last_name' => 'root',
-          'first_name' => 'root',
-          'email' => 'youremail@domain.com',
-          'password' => bcrypt('toor'),
+          'last_name' => env('ADMIN_LAST_NAME', 'root'),
+          'first_name' => env('ADMIN_FIRST_NAME', 'root'),
+          'email' => env('ADMIN_EMAIL', 'youremail@domain.com'),
+          'password' => bcrypt(env('ADMIN_PASSWORD', 'toor')),
           'level' => 0,
       ]);
       DB::unprepared(file_get_contents(app_path()."/database/seeds/suivisio_seeds.sql"));
