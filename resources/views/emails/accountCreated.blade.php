13703f349874f7a7df40dd2e3acb050ff0423b8a
Alexandre Meslé <alexandre.mesle@gmail.com>
2017-12-31 07:07:05 +0100

    Reformulation mail de création de compte.

--- resources/views/emails/accountCreated.blade.php
+++ resources/views/emails/accountCreated.blade.php
@@ -1,14 +1,7 @@
-<meta charset="utf-8">
-<meta name="description" content="">
-<meta name="author" content="Scotch">
-
-<!-- load bootstrap from a cdn -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css">
-
 <P>Bonjour {{ $user->fullName() }},</P>
 
 <P>Votre compte sur <a href="{{ config('app.url') }}">{{ config('app.name') }}</a> a bien été créé.
-Connectez-vous avec votre password <CODE>{{ config('app.defaultPassword') }}</CODE> (il vous sera demandé
+Connectez-vous avec le mot de passe par défaut <CODE>{{ config('app.defaultPassword') }}</CODE> (il vous sera demandé
 de le modifier à la première connexion).
 </P>
 
