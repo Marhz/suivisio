d140ac9a60b18826e82e47c1ba5f52933f76ab4a
Alexandre Meslé <alexandre.mesle@gmail.com>
2018-09-14 16:26:16 +0200

    Create a MacAdress resource.

--- resources/views/macAddresses/edit.blade.php
+++ resources/views/macAddresses/edit.blade.php
@@ -13,27 +13,18 @@
   @endif
 
 	<div class="panel panel-default">
-		<div class="panel-heading">
-        {{isset($address) ? "Modifier l'adresse MAC" : "Ajouter une adresse MAC"}}
-    </div>
+		<div class="panel-heading">Renseigner mon adresse MAC</div>
 		<div class="panel-body">
-      @if($user->isStudent() && $user->group->mac_address_deadline != null)
-        Verrouillage le
-                {{ (new Carbon($user->group->mac_address_deadline))->formatLocalized('%d/%m/%Y à %H heures') }}
-                ({{ (new Carbon($user->group->mac_address_deadline))->diffForHumans()}})
-        <hr>
-      @endif
-      @if(isset($address))
-        {{Form::open(['method' => 'put','url' => 'macAddress/'.$address->id])}}
-      @else
-        {{Form::open(['method' => 'post','url' => 'macAddress'])}}
-      @endif
+			{{Form::model($user,['method' => 'post','url' => 'macAddress'])}}
+        <input name="_token" type="hidden" id="_token" value="{{ csrf_token() }}" />
         <div class="form-group">
-          {{Form::label('address','Adresse MAC :')}}
-          {{Form::text('address',isset($address) ? $address->address : null, null,['id' => 'address', 'class' => 'form-control',''])}}
-          {{Form::submit('Enregistrer',['class' => 'btn btn-primary'])}}
-        </div>
-      {{Form::close()}}
+            {{Form::label('address','Adresse MAC :')}}
+            {{Form::text('address',$address, null,['id' => 'address', 'class' => 'form-control',''])}}
+				</div>
+        @can('editMacAddress', Auth::user())
+					{{Form::submit('Enregistrer',['class' => 'btn btn-primary form-control'])}}
+        @endcan
+			{{Form::close()}}
 		</div>
 	</div>
 @endsection
