b9274c052be74aa1931c59d95d248e17cc4b50a1
Alexandre Meslé <alexandre.mesle@gmail.com>
2018-09-21 23:26:11 +0200

    Mac address deadline.

--- resources/views/macAddresses/edit.blade.php
+++ resources/views/macAddresses/edit.blade.php
@@ -15,8 +15,17 @@
 	<div class="panel panel-default">
 		<div class="panel-heading">Renseigner mon adresse MAC</div>
 		<div class="panel-body">
-			{{Form::model($user,['method' => 'post','url' => 'macAddress'])}}
-        <input name="_token" type="hidden" id="_token" value="{{ csrf_token() }}" />
+      @if($user->isStudent() && $user->group->mac_address_deadline != null)
+        Verrouillage le
+                {{ (new Carbon($user->group->mac_address_deadline))->formatLocalized('%d/%m/%Y à %H heures') }}
+                ({{ (new Carbon($user->group->mac_address_deadline))->diffForHumans()}})
+        <hr>
+      @endif
+      @if(isset($address))
+        {{Form::open(['method' => 'put','url' => 'macAddress/'.$address->id])}}
+      @else
+        {{Form::open(['method' => 'post','url' => 'macAddress'])}}
+      @endif
         <div class="form-group">
             {{Form::label('address','Adresse MAC :')}}
             {{Form::text('address',$address, null,['id' => 'address', 'class' => 'form-control',''])}}
