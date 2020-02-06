8990e830bb86f5a03d931b9678ff76617c5b9f42
alexmf <alexmf@hotmail.com>
2020-02-06 02:58:09 +0100

    Réglages jenkins

--- docker/setup.sh
+++ docker/setup.sh
@@ -6,18 +6,21 @@ read_var() {
     echo ${VAR[1]}
 }
 
+git pull origin
 
 USER_ID=$(read_var USER_ID .env)
 USER_NAME=$(read_var USER_NAME .env)
 
 docker-compose build --build-arg USER_ID=$USER_ID --build-arg USER_NAME=$USER_NAME
 
+docker-compose down
+
 docker-compose up -d
 
 PHP_CONTAINER_NAME=$(read_var PHP_CONTAINER_NAME .env)
 
-docker exec -u $USER_NAME -ti $PHP_CONTAINER_NAME /usr/local/bin/composer install
+docker exec -u $USER_NAME -i $PHP_CONTAINER_NAME /usr/local/bin/composer install
 
-docker exec -u $USER_NAME -ti $PHP_CONTAINER_NAME php artisan migrate:refresh --seed
+docker exec -u $USER_NAME -i $PHP_CONTAINER_NAME php artisan migrate:refresh --seed
 
-docker exec -u $USER_NAME -ti $PHP_CONTAINER_NAME php artisan key:generate
+docker exec -u $USER_NAME -i $PHP_CONTAINER_NAME php artisan key:generate
