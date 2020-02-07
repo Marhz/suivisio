bc44753d209bcd3c88fa6dea76817304ad0567d6
alex <my@mail.com>
2020-02-07 15:15:19 +0100

    setup et update scripts

---
+++ docker/update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+read_var() {
+    VAR=$(grep $1 $2 | xargs)
+    IFS="=" read -ra VAR <<< "$VAR"
+    echo ${VAR[1]}
+}
+
+git pull origin
+
+USER_ID=$(read_var USER_ID .env)
+USER_NAME=$(read_var USER_NAME .env)
+
+docker-compose build --build-arg USER_ID=$USER_ID --build-arg USER_NAME=$USER_NAME
+
+docker-compose down
+
+docker-compose up -d
+
+PHP_CONTAINER_NAME=$(read_var PHP_CONTAINER_NAME .env)
+
+docker exec -u $USER_NAME -i $PHP_CONTAINER_NAME /usr/local/bin/composer install
+
