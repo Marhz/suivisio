3948f701e2ef2cb3808740ae07f5e2729226bcee
alex <my@mail.com>
2020-06-13 01:45:42 +0200

    [Mail] configuration adresse d'envoi

--- config/database.php
+++ config/database.php
@@ -114,6 +114,7 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
+	    'read_write_timeout' => 0,
         ],
 
     ],
